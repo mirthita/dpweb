108c7f1e42aa7e1e3f2d09beba78a8ca883a6f0f
mirthita <mirtha22.inicial@gmail.com>
2025-05-30 12:18:09 -0500

    prueba2

---
+++ config/config.example.php
@@ -0,0 +1,12 @@
+<?php
+const BD_HOST = 'localhost';
+const BD_NAME = '';
+const BD_USER = '';
+const BD_PASSWORD = '';
+const BD_charset = 'utf8';
+
+
+
+//ingresar link de proyecto
+const BD_url = '';
+?>
